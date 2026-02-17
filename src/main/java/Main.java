dc26876e66d49ac082b0c9becbbcc8049302b3a7
serbekun <sergey994049@gmail.com>
2026-02-17 20:30:29 +0900

    Add IdeasText in Core

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,40 +1,45 @@
 import Handles.Handles;
 import io.javalin.Javalin;
-import io.javalin.community.ssl.SslPlugin;
+// import io.javalin.community.ssl.SslPlugin;
 
 public class Main {
     
-
     public static void main(String[] args) {
 
         int port = 2000;
 
-        Javalin svr = Javalin.create(config -> {
-        config.registerPlugin(new SslPlugin(ssl -> {
-                // Load PEM certificates
-                try {
-                    ssl.pemFromPath("keys/fullchain.pem", "keys/privkey.pem");
+        // Javalin svr = Javalin.create(config -> {
+        // config.registerPlugin(new SslPlugin(ssl -> {
+        //         // Load PEM certificates
+        //         try {
+        //             ssl.pemFromPath("keys/fullchain.pem", "keys/privkey.pem");
 
-                } catch (Exception e) {
-                    System.err.println("[main] Error could not find keys files in 'keys/fullchain.pem & 'keys/privkey.pem'");
-                }
+        //         } catch (Exception e) {
+        //             System.err.println("[main] Error could not find keys files in 'keys/fullchain.pem & 'keys/privkey.pem'");
+        //         }
                 
-                // Configure HTTPS only
-                ssl.secure = true;
-                ssl.securePort = port;
-                ssl.insecure = false;
+        //         // Configure HTTPS only
+        //         ssl.secure = true;
+        //         ssl.securePort = port;
+        //         ssl.insecure = false;
                 
-                // HTTP/2 support
-                ssl.http2 = true;
-                ssl.sniHostCheck = false;
-            }));
-        });
+        //         // HTTP/2 support
+        //         ssl.http2 = true;
+        //         ssl.sniHostCheck = false;
+        //     }));
+        // });
+        
+        Javalin svr = Javalin.create();
 
         // initialize server endpoint handles
         Handles.InitHandles(svr);
 
-        // starting server
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Stopping server...");
+            svr.stop();
+        }));
+
         svr.start();
-    
+
     }
 }

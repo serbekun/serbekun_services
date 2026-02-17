cf72b5c92ae141dc0b6d091da51d0f0dffda02a5
serbekun <sergey994049@gmail.com>
2026-02-17 19:45:09 +0900

    init Javalin svr object with ssl

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,34 @@
+import io.javalin.Javalin;
+import io.javalin.community.ssl.SslPlugin;
+
 public class Main {
     
-    public static void main(String[] args) {
-        System.out.println("Setting up workspace complete");
-    }
 
+    public static void main(String[] args) {
+
+        int port = 2000;
+
+        Javalin svr = Javalin.create(config -> {
+        config.registerPlugin(new SslPlugin(ssl -> {
+                // Load PEM certificates
+                try {
+                    ssl.pemFromPath("keys/fullchain.pem", "keys/privkey.pem");
+
+                } catch (Exception e) {
+                    System.err.println("[main] Error could not find keys files in 'keys/fullchain.pem & 'keys/privkey.pem'");
+                }
+                
+                // Configure HTTPS only
+                ssl.secure = true;
+                ssl.securePort = port;
+                ssl.insecure = false;
+                
+                // HTTP/2 support
+                ssl.http2 = true;
+                ssl.sniHostCheck = false;
+            }));
+        });
+
+        
+    }
 }
